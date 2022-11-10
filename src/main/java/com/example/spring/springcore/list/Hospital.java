8a1639ba329c3a5daab039d266ce10c425a73cae
Utsav Dhakal <udhakal970@gmail.com>
2022-11-10 13:18:25 +0545

    injecting collection type :: list

---
+++ src/main/java/com/example/spring/springcore/list/Hospital.java
@@ -0,0 +1,32 @@
+package com.example.spring.springcore.list;
+
+import java.util.List;
+
+public class Hospital {
+    private String name;
+    private List<String> departments;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getDepartments() {
+        return departments;
+    }
+
+    public void setDepartments(List<String> departments) {
+        this.departments = departments;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "name='" + name + '\'' +
+                ", departments=" + departments +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/spring/springcore/list/Test.java
@@ -0,0 +1,12 @@
+package com.example.spring.springcore.list;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("list-config.xml");
+        Hospital hospital = ctx.getBean("hospital", Hospital.class);
+        System.out.println(hospital);
+    }
+}
