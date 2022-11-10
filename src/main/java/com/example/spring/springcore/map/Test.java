f3878beac16bc7e3777c94b17626754987eda95b
Utsav Dhakal <udhakal970@gmail.com>
2022-11-10 13:51:51 +0545

    injecting collection type :: map

---
+++ src/main/java/com/example/spring/springcore/map/Test.java
@@ -0,0 +1,11 @@
+package com.example.spring.springcore.map;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        var ctx = new ClassPathXmlApplicationContext("configs/map-config.xml");
+        Customer customer = ctx.getBean("customer", Customer.class);
+        System.out.println(customer);
+    }
+}
