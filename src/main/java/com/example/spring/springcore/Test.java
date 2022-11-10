60652c9a3ad8e32a0e5241c5b408407b58b34428
Utsav Dhakal <udhakal970@gmail.com>
2022-11-10 12:37:55 +0545

    dependency injection of primitive types

---
+++ src/main/java/com/example/spring/springcore/Test.java
@@ -0,0 +1,11 @@
+package com.example.spring.springcore;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        var ctx = new ClassPathXmlApplicationContext("config.xml");
+        Employee employee = ctx.getBean("emp", Employee.class);
+        System.out.println(employee);
+    }
+}
