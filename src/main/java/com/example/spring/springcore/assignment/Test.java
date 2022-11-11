37ab1f7dfa912fc5005e3ed54a9b6c620633706e
Utsav Dhakal <udhakal970@gmail.com>
2022-11-11 18:33:57 +0545

    *** setter injection with assignment completed ***

---
+++ src/main/java/com/example/spring/springcore/assignment/Test.java
@@ -0,0 +1,11 @@
+package com.example.spring.springcore.assignment;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        var ctx = new ClassPathXmlApplicationContext("configs/shop-config.xml");
+        ShoppingCart cart = ctx.getBean("cart", ShoppingCart.class);
+        System.out.println(cart);
+    }
+}
