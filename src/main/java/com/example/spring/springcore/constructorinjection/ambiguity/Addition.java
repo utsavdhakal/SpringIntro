e8abbbbb16d26bf097f42e1576a3bfd886e3da7b
Utsav Dhakal <udhakal970@gmail.com>
2022-11-13 19:25:38 +0545

    specifying constructor argument order using index attribute to prevent order ambiguity

--- src/main/java/com/example/spring/springcore/constructorinjection/ambiguity/Addition.java
+++ src/main/java/com/example/spring/springcore/constructorinjection/ambiguity/Addition.java
@@ -1,15 +1,8 @@
 package com.example.spring.springcore.constructorinjection.ambiguity;
 
 public class Addition {
-    public Addition(double a, double b) {
-        System.out.println(a + b);
-    }
-
-    public Addition(int a, int b) {
-        System.out.println(a + b);
-    }
-
-    public Addition(String a, String b) {
-        System.out.println(a + b);
+    public Addition(int a, double b) {
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
     }
 }
