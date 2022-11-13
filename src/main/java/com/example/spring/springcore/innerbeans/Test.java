a98dd5e651b93dc842934e119153e0b3209ec449
Utsav Dhakal <udhakal970@gmail.com>
2022-11-13 09:04:40 +0545

    scopes in action : singleton | prototype

--- src/main/java/com/example/spring/springcore/innerbeans/Test.java
+++ src/main/java/com/example/spring/springcore/innerbeans/Test.java
@@ -5,7 +5,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
     public static void main(String[] args) {
         var ctx = new ClassPathXmlApplicationContext("configs/innerbeans/config.xml");
+
         Employee employee = ctx.getBean("employee", Employee.class);
-        System.out.println(employee);
+        System.out.println(employee.hashCode());
+
+        Employee employee1 = ctx.getBean("employee", Employee.class);
+        System.out.println(employee1.hashCode());
     }
 }
